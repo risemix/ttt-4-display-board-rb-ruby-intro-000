0dec7c7de6b5719282db5a621e993bdd0cbabb2a
Evan K Pattison <epattison@gmail.com>
2020-03-06 22:42:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@
 # out the current state.
 board = ["O", " ", " ", "X", "X", " ", " ", " ", " "]
 def display_board(board)
-  print "   |   |   "
+  print " #{board[0]} | #{board[1]} | #{board[2]} "
   print "-----------"
-  print "   |   |   "
+  print " #{board[3]} | #{board[4]} | #{board[5]} "
   print "-----------"
-  print "   |   |   "
+  print " #{board[6]} | #{board[7]} | #{board[8]} "
 end
